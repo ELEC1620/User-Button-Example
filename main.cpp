5a542e151c4fd2373ead1c782979902ddeeb19f2
Andrew Knowles <87070974+AndrewJKnowles@users.noreply.github.com>
2021-07-13 09:57:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,25 +7,34 @@
 */
 
 #include "mbed.h"
-
-#define ON 1    //define 'ON' as 1
-#define OFF 0   //define 'OFF' as 0
  
-DigitalOut led(LED1);           //Initialise LED1 as a digital output
-DigitalIn button(BUTTON1);      //Initialise BUTTON1 as a digital input
+DigitalOut led(LED1);           //set LED1 as a digital output
+DigitalIn button(BUTTON1);      //set BUTTON1 as a digital input
+
+void init_buttons();
+void init_led();
 
 int main(){
+    init_buttons(); //initalise button
+    init_led();     //initalise led
 
     while(1){
 
         //read the status of the button, if true turn LED1 ON
         if(button.read() == true){
-            led = ON;
+            led.write(1);   //turn on led1
         }else{
-            led = OFF;
+            led.write(0);   //turn off led1
         };
 
 
     }
 }
 
+void init_buttons(){
+    button.mode(PullNone);  //turn off internal pull up / pull down resistors
+};
+
+void init_led(){
+    led.write(0);   //turn off led1
+};
